ac18a764e2a53a5edb4be3118c3ced9bc4bf4cd9
adrian zelada torrez <zelada_torrez@hotmail.com>
2019-08-29 01:39:12 -0400

    scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
